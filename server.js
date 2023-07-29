9d5e301fa78b57d7affc7f0c0f616ff0c9b09b85
Jonathan Roos <jonathanroos60@gmail.com>
2023-07-29 13:42:44 +1000

    added testroute.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app)
+var path = require('path');
+require('./routes/homeroute.js').route(app,path);
 
 app.use(express.static(__dirname+'/www'));
 
@@ -14,3 +16,4 @@ let server = http.listen(3000, function(){
 app.get('/test', function (req, res){
     res.sendFile(__dirname + '/www/test.html');
 });
+
