62c8b4df3a7fab9f6c16bade1b7d4467b87d804c
Jonathan Roos <jonathanroos60@gmail.com>
2023-07-29 15:21:18 +1000

    added form tempalte to form.html

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var http = require('http').Server(app)
 var path = require('path');
 require('./routes/homeroute.js').route(app,path);
+require('./routes/testroute.js').route(app,path);
 
 app.use(express.static(__dirname+'/www'));
 
@@ -11,9 +12,5 @@ let server = http.listen(3000, function(){
     let port = server.address().port;
     console.log("My first Nodejs Server!");
     console.log("Server listening on: "+ host + "port:" + port);
-});
-
-app.get('/test', function (req, res){
-    res.sendFile(__dirname + '/www/test.html');
 });
 
