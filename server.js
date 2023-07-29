d1f9ae176c6d078be7e031f4b3f708115929e958
Jonathan Roos <jonathanroos60@gmail.com>
2023-07-29 22:07:33 +1000

    script.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var path = require('path');
+
 app.use(bodyParser.json());
 // Import routes from ./routes directory
 require('./routes/home.js').route(app,path);
